3489327e5956cc7eaafa141b2917a0c80fec5f91
Vitalii Rakovtsii <mfthemon@gmail.com>
2017-06-05 15:45:12 +0300

    Fixing worker path

--- build/app/main.js
+++ build/app/main.js
@@ -4,7 +4,7 @@ const platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 const app_module_1 = require("./app.module");
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/worker.js', {
+    navigator.serviceWorker.register('worker.js', {
         scope: '/ua-web-challenge-xi-semifinal/'
     })
         .then((registration) => {
